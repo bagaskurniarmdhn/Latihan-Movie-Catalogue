9982f8788d98c1ee3b2097251eb1fae2e13df56a
bagaskurniarmdhn <99539620+bagaskurniarmdhn@users.noreply.github.com>
2022-05-05 20:19:51 +0700

    Add files via upload

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'ecae96a07e730c891c7e43b5054ce9e5',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
